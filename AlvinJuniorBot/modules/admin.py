bf520947f6a2ee5c399dfd922425ec7dc2221339
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-08 10:54:09 +0800

    Update admin.py

--- AlvinJuniorBot/modules/admin.py
+++ AlvinJuniorBot/modules/admin.py
@@ -5,15 +5,15 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from SaitamaRobot import DRAGONS, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (bot_admin, can_pin,
+from AlvinJuniorBot import SUDO_USERS , dispatcher
+from AlvinJuniorBot.modules.disable import DisableAbleCommandHandler
+from AlvinJuniorBot.modules.helper_funcs.chat_status import (bot_admin, can_pin,
                                                            can_promote,
                                                            connection_status,
                                                            user_admin,
                                                            ADMIN_CACHE)
 
-from SaitamaRobot.modules.helper_funcs.extraction import (extract_user,
+from AlvinJuniorBot.modules.helper_funcs.extraction import (extract_user,
                                                           extract_user_and_text)
 from SaitamaRobot.modules.log_channel import loggable
 from SaitamaRobot.modules.helper_funcs.alternate import send_message
@@ -36,7 +36,7 @@ def promote(update: Update, context: CallbackContext) -> str:
     promoter = chat.get_member(user.id)
 
     if not (promoter.can_promote_members or
-            promoter.status == "creator") and not user.id in DRAGONS:
+            promoter.status == "creator") and not user.id in SUDO_USERS :
         message.reply_text("You don't have the necessary rights to do that!")
         return
 
