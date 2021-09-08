1477a2d6873dc795dc20f6703becd259c466a1b4
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-08 11:18:18 +0800

    Update chatBois.py

--- AlvinJuniorBot/modules/chatBois.py
+++ AlvinJuniorBot/modules/chatBois.py
@@ -1,6 +1,6 @@
-from SaitamaRobot import pbot, arq, BOT_ID, DRAGONS
-from SaitamaRobot.utlis.errors import capture_err
-from SaitamaRobot.utlis.filter_groups import chatbot_group
+from AlvinJuniorBot import pbot, arq, BOT_ID, SUDO_USERS 
+from AlvinJuniorBot.utlis.errors import capture_err
+from AlvinJuniorBot.utlis.filter_groups import chatbot_group
 from pyrogram import filters
 
 
