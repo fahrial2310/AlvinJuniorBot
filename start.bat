d9d546c48920ca8f408a70d85bb07f226297f047
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-08 09:50:00 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE Alvin junior Bot
 :: Enables virtual env mode and then starts saitama
-env\scripts\activate.bat && py -mAlvinjuniorBot
+env\scripts\activate.bat && py -m AlvinjuniorBot
