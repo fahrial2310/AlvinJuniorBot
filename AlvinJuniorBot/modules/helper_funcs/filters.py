deb08c23144db9a3eac1774079102cc8c07690d9
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-08 10:12:42 +0800

    Update filters.py

--- AlvinJuniorBot/modules/helper_funcs/filters.py
+++ AlvinJuniorBot/modules/helper_funcs/filters.py
@@ -1,4 +1,4 @@
-from SaitamaRobot import DEV_USERS, DRAGONS, DEMONS
+from AlvinjuniorBot import DEV_USERS, SUDO_USERS , SUPPORT_USERS 
 from telegram import Message
 from telegram.ext import BaseFilter
 
@@ -8,14 +8,14 @@ class CustomFilters(object):
     class _Supporters(BaseFilter):
 
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in DEMONS)
+            return bool(message.from_user and message.from_user.id in SUPPORT_USERS )
 
     support_filter = _Supporters()
 
     class _Sudoers(BaseFilter):
 
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in DRAGONS)
+            return bool(message.from_user and message.from_user.id in SUDO_USERS)
 
     sudo_filter = _Sudoers()
 
